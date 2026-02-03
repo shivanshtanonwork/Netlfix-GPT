126dd5f24107129c6f4fd5bb4e226d9f5b782821
Shivansh Tandon <shivanshtandonwork@gmail.com>
2026-02-03 21:22:34 +0530

    custom hook

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,24 +1,9 @@
-import React, { useEffect } from "react";
 import Header from "./Header";
-import { API_OPTIONS } from "../utils/constants";
-import { useDispatch } from "react-redux";
-import { addNowPlayingMovies } from "../utils/moviesSlice";
+import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
 
 const Browse = () => {
-  const dispatch = useDispatch();
-  const getNowPlayingMovies = async () => {
-    const data = await fetch(
-      "https://api.themoviedb.org/3/movie/now_playing?page=1",
-      API_OPTIONS,
-    );
-    const json = await data.json();
-    console.log(json);
-    dispatch(addNowPlayingMovies(json.results));
-  };
+  useNowPlayingMovies();
 
-  useEffect(() => {
-    getNowPlayingMovies();
-  }, []);
   return (
     <div>
       <Header />
